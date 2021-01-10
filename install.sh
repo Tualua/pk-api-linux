c802ea95bad0b59e7446e29e476b7baf1c5d3363
Tualua <32481693+Tualua@users.noreply.github.com>
2021-01-10 10:13:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
+# Create spool directory
+mkdir -p /var/spool/zfsapi
+chown zfsreplica:www-data /var/spool/zfsapi
+
 # FreeBSD sudo is in /usr/local/bin and we need to make a link
 ln -s /usr/bin/sudo /usr/local/bin/sudo
